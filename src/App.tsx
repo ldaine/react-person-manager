7b9081e1e4ef010f81a55aea0124674bb2435004
Liga Daine <liga.daine@daine-it.com>
2019-04-07 15:41:44 +0200

    added sass to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ChangeEvent } from 'react';
-import './App.css';
+import './App.scss';
 import Person from './Person/Person'; 
 import { IPerson } from './models/IPerson';
 
